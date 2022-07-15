bc5deb28484e371ee5906417fb122539a6571d30
nicoriuki <nicoriuki@hotmail.com>
2022-07-15 01:35:24 -0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,38 +2,41 @@ import React from "react";
 
 const navBar = () => {
       return (
-            <div class="container">
-                  <nav class="navbar navbar-expand-lg bg-dark">
-                        <div class="container-fluid ">
-                              <a class="navbar-brand text-bg-dark p-3" href="#">
+            <div className="container">
+                  <nav className="navbar navbar-expand-lg bg-dark">
+                        <div className="container-fluid ">
+                              <a
+                                    className="navbar-brand text-bg-dark p-3"
+                                    href="#"
+                              >
                                     Consultorio
                               </a>
-                              <div class="d-flex justify-content-end">
+                              <div className="d-flex justify-content-end">
                                     <div
-                                          class="collapse navbar-collapse "
+                                          className="collapse navbar-collapse "
                                           id="navbarNav"
                                     >
-                                          <ul class="navbar-nav d-flex">
-                                                <li class="nav-item">
+                                          <ul className="navbar-nav d-flex">
+                                                <li className="nav-item">
                                                       <a
-                                                            class="nav-link text-bg-dark p-3"
+                                                            className="nav-link text-bg-dark p-3"
                                                             aria-current="page"
                                                             href="#"
                                                       >
                                                             Inicio
                                                       </a>
                                                 </li>
-                                                <li class="nav-item">
+                                                <li className="nav-item">
                                                       <a
-                                                            class="nav-link text-bg-dark p-3"
+                                                            className="nav-link text-bg-dark p-3"
                                                             href="#"
                                                       >
                                                             Pacientes
                                                       </a>
                                                 </li>
-                                                <li class="nav-item">
+                                                <li className="nav-item">
                                                       <a
-                                                            class="nav-link text-bg-dark p-3"
+                                                            className="nav-link text-bg-dark p-3"
                                                             href="#"
                                                       >
                                                             Agenda
