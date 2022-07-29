a18f1c907bd214dfb793d1c9a28c7285287bb758
nicoriuki <nicoriuki@hotmail.com>
2022-07-29 08:42:55 -0300

    detail

---
+++ src/components/paciente/PacienteDetail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PacienteDetail() {
+      return <div>PacienteDetail</div>;
+}
---
+++ src/components/paciente/PacienteDetailContainer.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export const PacienteDetailContainer = () => {
+      return (
+            <>
+                  <div>PacienteDetailContainer</div>
+            </>
+      );
+};
